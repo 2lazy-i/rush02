66833a6a2de6e798e353401d151e7ceba09f8c4a
Simon Wetting <simonwetting@hotmail.com>
2026-02-08 14:34:06 +0100

    Add files via upload

--- src/convert.c
+++ src/convert.c
@@ -1,26 +1,87 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   convert.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: 2lazy <2lazy@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/07 14:33:28 by 2lazy             #+#    #+#             */
-/*   Updated: 2026/02/07 15:32:14 by 2lazy            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   convert.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: 2lazy <2lazy@student.42.fr>                  +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/02/07 14:33:28 by 2lazy         #+#    #+#                 */
+/*   Updated: 2026/02/08 14:17:03 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rush02.h"
-
 // TODO:
 // 1. Split number into triads (groups of 3 digits)
 // 2. Skip "000" triads
 // 3. Call convert_triad() for each triad
 // 4. Append thousand/million/billion if needed
 // 5. Join triads into final string
+
+char *numb_gen(int amount)
+{
+	char	*number;
+	int		n;
+
+	number = malloc(amount + 2);
+	n = 0;
+	number[n++] = '1';
+	while (n <= amount)
+		number[n++] = '0';
+	number[n] = '\0';
+	return (number);
+}
+
+void	fill_triad(char triad[4], char *num)
+{
+	triad[0] = num[0];
+	triad[1] = num[1];
+	triad[2] = num[2];
+	triad[3] = '\0';
+}
+
+void	partial_triad(char triad[4], char *num, int lacking_numbers)
+{
+	int n;
+
+	triad[0] = '0';
+	if (lacking_numbers == 1)
+		triad[1] = num[0];
+		triad[2] = num[1];
+	if (lacking_numbers == 2)
+		triad[1] = '0';
+		triad[2] = num[0];
+	triad[3] = '\0';
+}
+
+
 char *convert_number(char *num, t_dict *dict)
 {
-    (void)num;
-    (void)dict;
-    return ft_strdup("zero"); // placeholder
+	char	buf[1000];
+    char	*result;
+	char	triad[4];
+	//char 	*number_zero;
+	int		length;
+	int		n;
+
+	result = buf;
+	length = ft_strlen2(num);
+	n = 0;
+	if (length % 3 != 0)
+	{
+		partial_triad(triad, num, length % 3);
+		printf("partial triad>%s\n", convert_triad(triad, dict));
+		n += length % 3;
+	}
+	while (n < length / 3)
+	{
+		fill_triad(triad, num + n * 3);
+		printf("triad>%s ", convert_triad(triad, dict));
+		if (length - n * 3 - 3 >= 3)
+			printf("(%d)>%s\n", length - n * 3 - 3, lookup(dict, numb_gen(length - n * 3 - 3)));
+		n++;
+	}
+	//printf("\ntest>%s ", convert_triad("400", dict));
+    return (NULL);
+    //return ft_strdup("zero"); // placeholder
 }
--- src/triad.c
+++ src/triad.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   triad.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: 2lazy <2lazy@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/07 14:33:44 by 2lazy             #+#    #+#             */
-/*   Updated: 2026/02/07 15:31:50 by 2lazy            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   triad.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: 2lazy <2lazy@student.42.fr>                  +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/02/07 14:33:44 by 2lazy         #+#    #+#                 */
+/*   Updated: 2026/02/08 14:20:48 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,46 @@
 // - Convert 0-999 triad to words
 // - Handle hundreds, 10-19 special case, tens, units
 // - Avoid extra spaces or unnecessary "zero"
+void	char_to_string(char *str, char c)
+{
+
+	str[0] = c;
+	str[1] = '\0';
+}
+
+char	*append_with_space(char *s1, char *s2)
+{
+	strcat(s1, " ");
+	strcat(s1, s2);
+	return  (s1);
+}
+
 char *convert_triad(char *triad, t_dict *dict)
 {
-    (void)triad;
-    (void)dict;
-    return NULL;
+	char	buf[200];
+	char	*result;
+    char	charstr[3];
+
+	result = buf;
+	if (triad[0] != '0')
+	{
+		char_to_string(charstr, triad[0]);
+		result = append_with_space(lookup(dict, charstr), lookup(dict, "100"));
+		result = strcat(result, " ");
+	}
+	if (triad[1] == '1')
+		return (strcat(result, lookup(dict, triad + 1)));
+	if (triad[1] == '0' && triad[2] == '0')
+		return (result);
+	if (triad[1] == '0')
+		return (strcat(result, lookup(dict, triad + 2)));
+	char_to_string(charstr, triad[1]);
+	strcat(charstr, "0");
+	result =  strcat(result, lookup(dict, charstr));
+	result = strcat(result, " ");
+	if (triad[2] == '0')
+		return (result);
+	char_to_string(charstr, triad[2]);
+	result =  strcat(result, lookup(dict, charstr));
+    return (result);
 }
